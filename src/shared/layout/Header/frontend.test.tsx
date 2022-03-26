d747384f36492f67d500d5532073ec3d347509fe
gustavo <gustavo.nogueira@dtidigital.com.br>
2022-03-26 14:44:42 -0300

    #12 finalizando testes do header

--- src/shared/layout/Header/frontend.test.tsx
+++ src/shared/layout/Header/frontend.test.tsx
@@ -1,6 +1,9 @@
 import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { ThemeProvider } from "styled-components";
 import Header from ".";
+import { IUserPosition } from "../../../Interfaces/interfaces";
+import { UserProfileContext } from "../../../providers/userProfile";
 import Theme from "../../../styles/theme";
 
 describe("Teste do componente <Header />", () => {
@@ -13,23 +16,100 @@ describe("Teste do componente <Header />", () => {
     cleanup();
   });
 
+  const mockUserProfile: IUserPosition = {
+    checkingAccountAmount: 100,
+    consolidated: 150,
+    positions: []
+  };
+
   test("Saldo deve estar na tela", async () => {
     render(
-      <ThemeProvider theme={Theme}>
-        <Header />
-      </ThemeProvider>
+      <UserProfileContext.Provider value={mockUserProfile}>
+        <ThemeProvider theme={Theme}>
+          <Header />
+        </ThemeProvider>
+      </UserProfileContext.Provider>
     );
 
     expect(screen.getByText("Saldo:", { exact: true })).toBeInTheDocument();
+    expect(
+      screen.getByText(mockUserProfile.checkingAccountAmount, { exact: false })
+    ).toBeInTheDocument();
   });
 
   test("Patrimonio deve estar na tela", async () => {
     render(
-      <ThemeProvider theme={Theme}>
-        <Header />
-      </ThemeProvider>
+      <UserProfileContext.Provider value={mockUserProfile}>
+        <ThemeProvider theme={Theme}>
+          <Header />
+        </ThemeProvider>
+      </UserProfileContext.Provider>
     );
 
     expect(screen.getByText("Patrimônio:", { exact: true })).toBeInTheDocument();
+    expect(screen.getByText(mockUserProfile.consolidated, { exact: false })).toBeInTheDocument();
+  });
+
+  test("Botões de ocultar devem estar na tela", async () => {
+    render(
+      <UserProfileContext.Provider value={mockUserProfile}>
+        <ThemeProvider theme={Theme}>
+          <Header />
+        </ThemeProvider>
+      </UserProfileContext.Provider>
+    );
+
+    const buttons = screen.queryAllByRole("button");
+    expect(buttons).toHaveLength(2);
+  });
+
+  test("Saldo deve ser ocultado conforme clique no botao de ocultar", async () => {
+    render(
+      <UserProfileContext.Provider value={mockUserProfile}>
+        <ThemeProvider theme={Theme}>
+          <Header />
+        </ThemeProvider>
+      </UserProfileContext.Provider>
+    );
+
+    const buttons = screen.queryAllByRole("button");
+
+    //saldo deve estar presente na tela
+    expect(screen.getByText("Saldo:", { exact: true })).toBeInTheDocument();
+    expect(
+      screen.getByText(mockUserProfile.checkingAccountAmount, { exact: false })
+    ).toBeInTheDocument();
+
+    //clicando no botao
+    userEvent.click(buttons[0]);
+
+    //saldo não deve estar mais presente na tela após clique no botao
+    expect(
+      screen.queryByText(mockUserProfile.checkingAccountAmount, { exact: false })
+    ).not.toBeInTheDocument();
+  });
+
+  test("Patrimônio deve ser ocultado conforme clique no botao de ocultar", async () => {
+    render(
+      <UserProfileContext.Provider value={mockUserProfile}>
+        <ThemeProvider theme={Theme}>
+          <Header />
+        </ThemeProvider>
+      </UserProfileContext.Provider>
+    );
+
+    const buttons = screen.queryAllByRole("button");
+
+    //patrimonio deve estar presente na tela
+    expect(screen.getByText("Patrimônio:", { exact: true })).toBeInTheDocument();
+    expect(screen.getByText(mockUserProfile.consolidated, { exact: false })).toBeInTheDocument();
+
+    //clicando no botao
+    userEvent.click(buttons[1]);
+
+    //patrimonio não deve estar mais presente na tela após clique no botao
+    expect(
+      screen.queryByText(mockUserProfile.consolidated, { exact: false })
+    ).not.toBeInTheDocument();
   });
 });
